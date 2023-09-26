bf8c1ce518f683e084699cd4bddaab9dbdf354fb
damienlebre <lebre.damien88@gmail.com>
2023-09-26 16:45:45 +0200

    second

---
+++ src/main/java/com/humanbooster/exam/Thymeleaf/Repository/LigneFactureRepository.java
@@ -0,0 +1,11 @@
+package com.humanbooster.exam.Thymeleaf.Repository;
+
+import com.humanbooster.exam.Thymeleaf.Models.LigneFacture;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface LigneFactureRepository extends CrudRepository<LigneFacture, Long> {
+
+
+}
